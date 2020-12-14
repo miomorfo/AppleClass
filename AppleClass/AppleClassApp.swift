b0583f7420c303de4354084a07cf7c8ea4f5d178
fescobab <fescobab@everis.com>
2020-12-14 17:46:47 -0300

    Initial Commit

---
+++ AppleClass/AppleClassApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleClassApp.swift
+//  AppleClass
+//
+//  Created by fescobab on 14-12-20.
+//
+
+import SwiftUI
+
+@main
+struct AppleClassApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
