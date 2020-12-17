26ed82af7922640ca97f3d0aadc78f4b7f0e4eed
fescobab <fescobab@everis.com>
2020-12-17 13:04:12 -0300

    se añade visualizacion con diferentes dispositivos

--- AppleClass/Views/LandmarkList.swift
+++ AppleClass/Views/LandmarkList.swift
@@ -25,5 +25,12 @@ struct LandmarkList: View {
 struct LanfmarkList_Previews: PreviewProvider {
     static var previews: some View {
         LandmarkList()
+        // proporcionar el dispositivo a redenrizar
+//        ForEach(["iPhone SE (2nd generation)", "iPhone XS Max", "iPad Pro (11-inch) (2nd generation)"], id: \.self) {
+//            deviceName in LandmarkList()
+//                .previewDevice(PreviewDevice(rawValue: deviceName))
+//                .previewDisplayName(deviceName)
+//        }
+            
     }
 }
