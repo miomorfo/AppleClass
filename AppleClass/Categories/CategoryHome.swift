1309d25036a3fb0015bbede83463e66d04bf473c
Fernando <fernando.eb@icloud.com>
2020-12-18 19:17:14 -0300

    se añade vista de tabs

---
+++ AppleClass/Categories/CategoryHome.swift
@@ -0,0 +1,39 @@
+//
+//  CategoryHome.swift
+//  AppleClass
+//
+//  Created by Fernando on 18-12-20.
+//
+
+import SwiftUI
+
+
+struct CategoryHome: View {
+	@EnvironmentObject var modelData: ModelData
+	
+	var body: some View {
+		NavigationView {
+			List {
+				modelData.features[0].image
+					.resizable()
+					.scaledToFill()
+					.frame(height: 200)
+					.clipped()
+					.listRowInsets(EdgeInsets())
+
+				
+				ForEach(modelData.categories.keys.sorted(), id: \.self) { key in
+					CategoryRow(categoryName: key, items: modelData.categories[key]!)
+				}
+				.listRowInsets(EdgeInsets())
+			}
+			.navigationTitle("Featured")
+		}
+	}
+}
+struct CategoryHome_Previews: PreviewProvider {
+	static var previews: some View {
+		CategoryHome()
+			.environmentObject(ModelData())
+	}
+}
